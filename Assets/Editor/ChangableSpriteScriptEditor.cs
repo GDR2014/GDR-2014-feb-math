a1eaf9653853527ece0d7172e6f28807aa106493
KHReenberg <khreenberg@gmail.com>
2014-02-15 10:27:52 +0100

    Adds a script for changing sprites easily + editor for said script

---
+++ Assets/Editor/ChangableSpriteScriptEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor( typeof( ChangableSpriteScript ) )]
+public class ChangableSpriteScriptEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        ChangableSpriteScript script = (ChangableSpriteScript) target;
+        if( GUI.changed ) {
+            script.UpdateSprite();
+            GUI.changed = false;
+        }
+    }
+}
---
+++ Assets/Scripts/ChangableSpriteScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent( typeof( SpriteRenderer ) )]
+public class ChangableSpriteScript : MonoBehaviour {
+
+    public int spriteIndex = 0;
+    public Sprite[] sprites;
+
+    protected new SpriteRenderer renderer;
+
+    // Use this for initialization
+    private void Start() {
+        renderer = GetComponent<SpriteRenderer>();
+        if( spriteIndex >= 0 && spriteIndex < sprites.Length )
+            UpdateSprite();
+    }
+
+    // Update is called once per frame
+    public void UpdateSprite() {
+        renderer.sprite = sprites[spriteIndex];
+    }
+}

50d7f5ddb21fb516ada8f9ab5670f8192ec98014
KHReenberg <khreenberg@gmail.com>
2014-02-21 15:38:29 +0100

    Stuff reacts to user input. Only '+' is generated, and player can only attack to the left

---
+++ Assets/DebugScript.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class DebugScript : MonoBehaviour {
+    void OnGUI() {
+        if( Event.current.keyCode == KeyCode.R )
+            Application.LoadLevel( 0 );
+    }
+}
